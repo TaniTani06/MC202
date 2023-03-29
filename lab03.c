4aaf71a4e44c587909ac04e961bad84243632d29
TaniTani06 <gustavoandradetaniguchi@gmail.com>
2023-03-29 11:11:42 -0300

    edited lab03.c file

--- lab03.c
+++ lab03.c
@@ -1,5 +1,41 @@
 #include<stdio.h>
 
+int minimo(int* lista, int n_atend);
+
+int main(){
+    int atendentes;
+    int clientes;
+    int chegada;
+    int atendimento;
+    int espera;
+    int mais20;
+    int livre[101] = {0};
+
+    mais20 = 0;
+
+    scanf("%d %d", &atendentes, &clientes);
+    for (int linha=0; linha <atendentes; linha++){
+        scanf("%d %d", &chegada, &atendimento);
+        livre[linha] = chegada + atendimento;
+    }
+
+    for (int novalinha = atendentes; novalinha < clientes; novalinha++){
+        scanf("%d %d", &chegada, &atendimento);
+        if (chegada < livre[minimo(livre,atendentes)]){
+            espera = livre[minimo(livre,atendentes)] - chegada;
+            if (espera>=20){
+                mais20++;
+            }
+            livre[minimo(livre,atendentes)] += atendimento;
+        }
+        else{
+            livre[minimo(livre,atendentes)] = chegada + atendimento;
+        }
+    }
+
+    printf("%d\n", mais20);    
+}
+
 int minimo(int* lista, int n_atend){
     int min = 0;
     for (int i=1; i < n_atend; i++){
@@ -13,24 +49,3 @@ int minimo(int* lista, int n_atend){
         }
     }
 }
-
-int main(){
-    int atendentes;
-    int clientes;
-    int chegada;
-    int atendimento;
-    int livre[101] = {0};
-
-    scanf("%d %d", &atendentes, &clientes);
-    for (int linha=0; linha < atendentes; linha++){
-        scanf("%d %d", &chegada, &atendimento);
-        livre[linha] = chegada + atendimento;
-    }
-    
-    for (linha = atendentes; linha < clientes){
-        scanf("%d %d", &chegada, &clientes);
-    }
-    
-}
-
-
