3c9b32c24367f79988a12cc11d5b11724f9995f6
TaniTani06 <gustavoandradetaniguchi@gmail.com>
2023-04-13 13:07:56 -0300

    Lapidação

--- lab04.c
+++ lab04.c
@@ -10,11 +10,7 @@ int main(void){
     char pessoa_1[51], pessoa_2[51];
     int conhecimento[1001][1001];
     int i = 0;
-    int p;
-    int q;
-    int k;
-    int l;
-    int m;
+    int p, q, k, l, m, tamanho, linha, coluna;
 
     for (i=0; i<1001; i++){
         pessoas[i][0] = '\0';
@@ -23,9 +19,6 @@ int main(void){
     while (continua == 1){
         char* pos = strstr(frase, " conhece ");
         int loc = pos - frase;                          //local do " conhece " na linha
-
-        memset(pessoa_1, 0, 51);                        //"limpa" as strings
-        memset(pessoa_2, 0, 51);
 
         strncpy(pessoa_1, frase, loc);
         pessoa_1[loc] = '\0';
@@ -38,42 +31,53 @@ int main(void){
             else{
                 if (pessoas[p][0] == '\0'){
                     strcpy(pessoas[p], pessoa_1);
+                    tamanho = p+1;
                     break;
                 }
             }
         }
-
+        
         for (q=0; q<1001; q++){                     //verifica se "pessoa_2" está na matriz e adiciona se não estiver
             if (strcmp(pessoas[q], pessoa_2) == 0){
                 break;
             }
             else{
-                if (pessoas[q][1] == 0){
+                if (pessoas[q][1] == '\0'){
                     strcpy(pessoas[q], pessoa_2);
+                    tamanho = q+1;
                     break;
                 }
             }
         }
-
+        
         conhecimento[p][q] = 1;                    //registra que pessoas[p] conhece pessoas[q]
         
         i++;
         continua = scanf("%[^\n] ", frase);
     }
     
-    for (k=0; k<q+1; k++){
-        for (l=0; l<q+1; l++){
+    for (k=0; k<tamanho; k++){
+        for (l=0; l<tamanho; l++){
+            coluna = l;
             if (conhecimento[k][l] == 1){           //verifica se há "1" na linha
                 break;
             }
         }
-        if (conhecimento[k][l] == 0){               //ao sair do loop interno, caso ele seja concluído, verficamos a coluna
-            for (m=0; m<q-1; m++){
+        
+        if (conhecimento[k][coluna] == 0){          //ao sair do loop interno, caso ele seja concluído, verficamos a coluna
+            for (m=0; m<k; m++){
+                linha = m;
                 if (conhecimento[m][k] == 0){
                     break;
                 }
             }
-            if (conhecimento[m][k] == 1){
+            for (m=k+1; m<tamanho; m++){
+                linha = m;
+                if (conhecimento[m][k] == 0){
+                    break;
+                }
+            }
+            if (conhecimento[linha][k] == 1){
                 printf("%s e' celebridade.\n", pessoas[k]);
                 return 0;
             }
