47e4212667cd2113bf8e3a296bae58362070836e
TaniTani06 <gustavoandradetaniguchi@gmail.com>
2023-04-29 09:50:03 -0300

    Finished value read function and added p function

--- lab05.c
+++ lab05.c
@@ -1,26 +1,43 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct{
     int linha;
     int coluna;
     int valor;
-} vetorc;
+} elemento;
 
+elemento *v_alloc(int n){
+    elemento *p = (elemento*) malloc(n * sizeof(elemento));
+    return p;
+
+}
+
+int p(elemento* vetor, int tamanho){              //função p, imprime o conteúdo do VC, retorna 1(falha) ou 0 (sucesso);
+    printf("VC: ");
+    
+    for (int i = 0; i < tamanho; i++){
+    printf("(%d,%d,%d) ", vetor[i].linha, vetor[i].coluna, vetor[i].valor);
+    }
+    
+    printf("\n");
+    return 0;
+}
 
 int main(){
 
 int m, n, k;
+char str[15];
 
-scanf("%d %d", m, n);
-scanf ("%d", k);
+scanf("%d %d\n", &m, &n);
+scanf ("%d\n", &k);
 
-struct VC[m+n];
-
-for (int i = 0; i < k; i++){
-    scanf("[%d,%d] %d", VC[i].linha, VC[i].coluna, VC[i].valor);
-}
+elemento *VC = v_alloc(m+n);
 
 for (int i = 0; i < k; i++){
-    printf("(%d,%d,%d)\n", VC[i].linha, VC[i].coluna, VC[i].valor);
+    scanf("[%d,%d] %d\n", &VC[i].linha, &VC[i].coluna, &VC[i].valor);
 }
+
+p(VC, k);
+
 }
